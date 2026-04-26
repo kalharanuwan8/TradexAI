bb25d23e46271fe1bb000facbd4baf31519d5ce0
kalharanuwan8 <91245903+kalharanuwan8@users.noreply.github.com>
2026-04-26 13:12:25 +0530

    Testing 1

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'https://tradex-backend.niceriver-7d6bf361.southeastasia.azurecontainerapps.io/',
         changeOrigin: true,
       },
     },
